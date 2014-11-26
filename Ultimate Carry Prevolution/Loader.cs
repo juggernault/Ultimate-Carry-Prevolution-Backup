88d5d5f1e0a2f7d6504fb1db66e85ccc91b9b082
NitroXenon <peter9104@hotmail.com>
2014-11-26 17:15:42 +0800

    Push source code.

---
+++ Ultimate Carry Prevolution/Loader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LeagueSharp;
+
+namespace Ultimate_Carry_Prevolution
+{
+    class Loader
+    {
+        public const double VersionNumber = 0.7;
+        public Loader()
+        {
+          Chat.WellCome();
+        }
+    }
+}
---
+++ Ultimate Carry Prevolution/Plugin/Gnar.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using xSLx_Orbwalker;
+using Color = System.Drawing.Color;
+
+namespace Ultimate_Carry_Prevolution.Plugin
+{
+    class Gnar : Champion
+    {
+
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    ///////////////                ///////////////
+    ///////////////  File Missing  ///////////////
+    ///////////////                ///////////////
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    
+    }
+}
